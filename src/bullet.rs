50f99df368b3753b74b2dfbd9d38822d8cd5118f
Ellie <lewisfulterer@gmail.com>
2026-09-20 15:53:54 +0100

    splatting speedup

--- src/bullet.rs
+++ src/bullet.rs
@@ -4,36 +4,46 @@ use bulletformat::ChessBoard;
 
 use crate::{
     board::Board,
-    common::{Color, Move, Piece},
+    common::{Color, Move, Piece, Square},
     format::{self, GameOutcome, MoveRecord},
 };
 
 pub const DUCK_EXTRA_INDEX: usize = 0;
 
 pub fn to_bulletformat(board: &Board, white_score: i16, outcome: GameOutcome) -> io::Result<ChessBoard> {
-    let bbs = [
-        board.colors(Color::White).0,
-        board.colors(Color::Black).0,
-        board.pieces(Piece::Pawn).0,
-        board.pieces(Piece::Knight).0,
-        board.pieces(Piece::Bishop).0,
-        board.pieces(Piece::Rook).0,
-        board.pieces(Piece::Queen).0,
-        board.pieces(Piece::King).0,
-    ];
-    if (bbs[0] | bbs[1]).count_ones() > 32
-        || (bbs[0] & bbs[7]).count_ones() != 1
-        || (bbs[1] & bbs[7]).count_ones() != 1
+    let us = board.colors(board.stm()).0;
+    let them = board.colors(!board.stm()).0;
+    let kings = board.pieces(Piece::King).0;
+    let occupied = us | them;
+    if occupied.count_ones() > 32
+        || (us & kings).count_ones() != 1
+        || (them & kings).count_ones() != 1
     {
         return Err(format::invalid("expected at most 32 chess pieces and both kings"));
     }
 
     let black = board.stm() == Color::Black;
-    let mut position = ChessBoard::from_raw(bbs, usize::from(black), 0, outcome as u8 as f32 / 2.0)
-        .map_err(format::invalid)?;
-    position.score = if black { white_score.saturating_neg() } else { white_score };
-    position.extra[DUCK_EXTRA_INDEX] = board.duck()
-        .map_or(format::NO_SQUARE, |sq| sq as u8 ^ if black { 56 } else { 0 });
+    let flip = if black { 56 } else { 0 };
+    let mut position = ChessBoard {
+        occ: if black { occupied.swap_bytes() } else { occupied },
+        pcs: [0; 16],
+        score: if black { white_score.saturating_neg() } else { white_score },
+        result: if black { 2 - outcome as u8 } else { outcome as u8 },
+        ksq: (us & kings).trailing_zeros() as u8 ^ flip,
+        opp_ksq: (them & kings).trailing_zeros() as u8 ^ flip ^ 56,
+        extra: [board.duck().map_or(format::NO_SQUARE, |sq| sq as u8 ^ flip), 0, 0],
+    };
+    let mut remaining = position.occ;
+    let mut index = 0;
+    while remaining != 0 {
+        let square = remaining.trailing_zeros() as u8 ^ flip;
+        remaining &= remaining - 1;
+        let piece = board.piece_on(Square::index(usize::from(square)))
+            .ok_or_else(|| format::invalid("occupied square has no piece"))? as u8;
+        let colour = u8::from(them & (1u64 << square) != 0) << 3;
+        position.pcs[index / 2] |= (piece | colour) << (4 * (index & 1));
+        index += 1;
+    }
     Ok(position)
 }
 
